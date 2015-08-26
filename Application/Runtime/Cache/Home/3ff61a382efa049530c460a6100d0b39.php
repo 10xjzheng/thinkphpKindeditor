f02db4144a09852eaeba7f8c6fece01e51a2fb74
10xjzheng <10xjzheng@gmail.com>
2015-08-26 21:48:32 +0800

    【add】add a new project

---
+++ Application/Runtime/Cache/Home/3ff61a382efa049530c460a6100d0b39.php
@@ -0,0 +1,12 @@
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="utf-8">
+<title>kindeditor</title>
+<link href="/github/thinkphpKindeditor/Public/bootstrap/css/bootstrap.css" rel="stylesheet">
+<script src="/github/thinkphpKindeditor/Public/js/jquery.min.js"></script>
+<script src="/github/thinkphpKindeditor/Public/plugins/kindeditor/kindeditor.js"></script>
+</head>
+<div><?php echo ($detail); ?></div>
+</body>
+</html>
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # thinkphpKindeditor
 thinkphp+kindeditor
+
+![res1](/img/res1.png)
+![res2](/img/res2.png)
